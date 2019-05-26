3afaa58837622a1cc6da4f64cd202c64417783bf
alxfed <alxfed@gmail.com>
2019-05-26 11:04:54 -0500

    item

--- ccrecorder/ccrecorder/spiders/records.py
+++ ccrecorder/ccrecorder/spiders/records.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from scrapy.spiders import CSVFeedSpider
+from ccrrecorder.items import CCrecord
 
 
 class RecordsSpider(CSVFeedSpider):
     name = 'records'
     allowed_domains = ['ccrecorder.org']
     start_urls = ['https://alxfed.github.io/docs/pin_feed.csv']
+    prefix_url = 'https://www.ccrecorder.org/parcels/search/parcel/result/?line='
     headers = ['pin']
     # delimiter = '\t'
 
@@ -15,4 +17,10 @@ class RecordsSpider(CSVFeedSpider):
 
     def parse_row(self, response, row):
         pin = row['pin']
-        return pin
+        return scrapy.Request(prefix_url+pin, callback=self.parse_pin_page)
+
+    def parse_pin_page(self, response):
+        if response.xpath('//.../text()').extract() == ...
+            yield None
+
+        item = CC
