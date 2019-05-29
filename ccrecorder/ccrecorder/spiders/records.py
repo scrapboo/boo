698616fd50bb2410b09d0671eced2200618af73d
alxfed <alxfed@gmail.com>
2019-05-29 15:12:47 -0500

    record page

--- ccrecorder/ccrecorder/spiders/records.py
+++ ccrecorder/ccrecorder/spiders/records.py
@@ -17,9 +17,9 @@ class RecordsSpider(CSVFeedSpider):
     def parse_row(self, response, row):                                 # parse a row in CSV
         PIN_REQUEST_URL = 'https://www.ccrecorder.org/parcels/search/parcel/result/?line='
         pin = row['pin']                                                # the name of the column defined in 'headers'
-        return scrapy.Request(url=PIN_REQUEST_URL + pin, callback=self.parse_pin_page)
+        return scrapy.Request(url=PIN_REQUEST_URL + pin, callback=self.parse_pin_page(self, response, pin))
 
-    def parse_pin_page(self, response):
+    def parse_pin_page(self, response, pin):
         DOCUMENTS_PAGE_URL = 'https://www.ccrecorder.org/parcels/show/parcel/'
         RECORD_NUMBER_XPATH = '//*[@id="objs_body"]/tr/td[4]/a/@href'
         NO_PINS_FOUND_RESPONSE_XPATH = '//html/body/div[4]/div/div/div[2]/div/div/p[2]/text()' # where it can be
@@ -37,7 +37,7 @@ class RecordsSpider(CSVFeedSpider):
             record = CCrecord()                                         # import the scrapy.Item container.
             record_number = response.xpath(RECORD_NUMBER_XPATH).re('[.0-9]+')[0]
             record['record_number'] = record_number
-            # self.log(record)     # the movable debug line.
+            self.log(pin)     # the movable debug line.
             yield scrapy.Request(url=DOCUMENTS_PAGE_URL + record_number + '/', callback=self.parse_docs_page)
 
     def parse_docs_page(self, response):
