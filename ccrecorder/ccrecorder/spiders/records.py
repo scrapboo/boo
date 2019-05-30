13dc302788b5055d3c3924d6667901039bb003fc
alxfed <alxfed@gmail.com>
2019-05-30 08:27:05 -0500

    record page

--- ccrecorder/ccrecorder/spiders/records.py
+++ ccrecorder/ccrecorder/spiders/records.py
@@ -5,21 +5,36 @@ from ccrecorder.items import CCrecord
 
 
 class RecordsSpider(CSVFeedSpider):
+    """
+    Reads the pin feed csv file and parses the CC recorder
+    for these pins.
+    The pins can be 10 or 14 digit long
+    """
     name = 'records'
     allowed_domains = ['ccrecorder.org']
     start_urls = ['https://alxfed.github.io/docs/pin_feed.csv']
     headers = ['pin']
 
-    # Do any adaptations you need here
-    #def adapt_response(self, response):
-    #    return response
-
-    def parse_row(self, response, row):                                 # parse a row in CSV
+    def parse_row(self, response, row):
+        """
+        Reads a row in csv and makes a request to the pin page
+        :param response: the downloaded pin page
+        :param row: a single PIN read from the pin feed file
+        :return: yiels a scrapy.Item
+        """
         PIN_REQUEST_URL = 'https://www.ccrecorder.org/parcels/search/parcel/result/?line='
         pin = row['pin']                                                # the name of the column defined in 'headers'
         return scrapy.Request(url=PIN_REQUEST_URL + pin, callback=self.parse_pin_page, meta={'pin':pin})
 
     def parse_pin_page(self, response):
+        """
+        Parses the pin page, detects if there is none, detects if
+        there are multiple pins and their corresponding links on a page,
+        jumps to those pages and scrapes their contence into a
+        scrapy.item CCrecord
+        :param response:
+        :return: yields a record of a bunch of records
+        """
         DOCUMENTS_PAGE_URL = 'https://www.ccrecorder.org/parcels/show/parcel/'
         RECORD_NUMBER_XPATH = '//*[@id="objs_body"]/tr/td[4]/a/@href'
         NO_PINS_FOUND_RESPONSE_XPATH = '//html/body/div[4]/div/div/div[2]/div/div/p[2]/text()' # where it can be
@@ -45,6 +60,11 @@ class RecordsSpider(CSVFeedSpider):
                                  )
 
     def parse_docs_page(self, response):
+        """
+        Parces the docs page after jumping to it.
+        :param response:
+        :return: yield a scrapy.item CCrecord for every valid PIN
+        """
         record = CCrecord()
         record['pin'] = response.meta['pin']
         record['record_number'] = response.meta['record_number']
